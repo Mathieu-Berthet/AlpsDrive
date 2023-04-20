117c4b3af5ff5762e7557bed51e2f240b28e9250
Mathieu <berthetmathieu@free.fr>
2023-04-20 16:27:45 +0200

    try to get folders and file

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use(express.static('frontend'));
 
 
 let myRegex = /^[a-zA-Z]+$/;
-//console.log(os.tmpdir());
+
 app.post('/api/drive', (req, res, next) => {
-    console.log("coucou");
-    console.log(req.query.name);
     if(myRegex.test(req.query.name))
     {
         console.log("test réussi");
@@ -34,31 +32,20 @@ app.post('/api/drive', (req, res, next) => {
         console.log("Test echoué");
         res.status(400);
     }
-    next();
+    //next();
 });
 
 app.get('/api/drive' , (req, res, next) => {
     let folders = {};
-    /*const folders = [
-        {
-            name: "Dossier",
-            isFolder: true,
-        },
-        {
-            name: "fichier",
-            size: 1204,
-            isFolder: false,
-        },
-    ];
-    res.status(200).json(folders);*/
-    fs.readdir(os.tmpdir(), (error, files) => {
+    let folderString;
+
+    fs.readdir(os.tmpdir(),(error, files) => {
         console.log("lecture");
-        console.log("fichier : ", files);
-        console.log("obj : ", folders);
-        folders = files;
-        console.log("obj rempli : ", folders);
+        res.status(200).json(files);
     });
-   res.status(200).json(folders);
+    //res.status(200).json(folders);
+    //folderString = fs.readdirSync(os.tmpdir());
+    //res.status(200).json(files);
 });
 
 
