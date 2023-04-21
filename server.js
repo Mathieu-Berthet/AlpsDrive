921371dee72d56c7aaf0486395cafe1c2f3018f3
Mathieu <berthetmathieu@free.fr>
2023-04-21 14:50:34 +0200

    get info for one file or directory

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.post('/api/drive', (req, res, next) => {
 app.get('/api/drive' , async (req, res, next) => {
 
     let files = await fs.promises.readdir(os.tmpdir(), {withFileTypes : true});
-    //fs.statSync(os.tmpdir())// Pour la size
     const fileList = files.map((fileName) => {
         if(fileName.isDirectory())
         {
@@ -53,12 +52,55 @@ app.get('/api/drive' , async (req, res, next) => {
             return {
                 "name" : fileName.name,
                 "isFolder" : fileName.isDirectory(),
-                "size" : fs.statSync(os.tmpdir())['size']// Pour la size
+                "size" : fs.statSync(os.tmpdir() + "/" + fileName.name)['size']// Pour la size
             }
         }
     });
 
     res.status(200).json(fileList);
+
+});
+
+app.get('/api/drive/:name', (req, res, next) => {
+    let nameFile = req.params.name;
+    if(fs.existsSync(os.tmpdir() + "/" + nameFile))
+    {
+        fs.stat(os.tmpdir() + "/" + nameFile, async(err, fold) => {
+            if (fold.isDirectory())
+            {
+                //Lire un dossier
+                let folders = await fs.promises.readdir(os.tmpdir() + "/" + nameFile, {withFileTypes: true});
+                const folderList = folders.map((folderName) => {
+                    if (folderName.isDirectory())
+                    {
+                        return {
+                            "name": folderName.name,
+                            "isFolder": folderName.isDirectory(),
+                        }
+                    }
+                    else
+                    {
+                        return {
+                            "name": folderName.name,
+                            "isFolder": folderName.isDirectory(),
+                            "size": fs.statSync(os.tmpdir() + "/" + nameFile + "/" + folderName.name)['size']// Pour la size
+                        }
+                    }
+                });
+                res.status(200).json(folderList);
+            }
+            else
+            {
+                //Lire un fichier
+                let myFile = fs.readFileSync(os.tmpdir() + "/" + nameFile, {encoding: 'utf8'});
+                res.status(200).send(myFile);
+            }
+        })
+    }
+    else
+    {
+        res.status(404).message('Le fichier ou répertoire n\'existe pas');
+    }
 
 });
 
